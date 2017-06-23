cf407f04e73dfde7ca7f7836c042f1aba6ad49b8
SHYANG-ENVY\shyang.oblab <shyang.oblab@gmail.com>
2017-06-23 23:15:30 +0900

    대여료 계산 메서드 옮기기
    Customer 클래스의 amountFor 메서드는 Customer를 사용하지 않는다
    인자로 받아서 사용하고 있는 Rental 클래스로 옮긴다
    메서드 이동 기법 - Move Method 
    * amountFor -> getCharge
    * aRental 매개변수 삭제

--- src/main/java/com/example/Rental.java
+++ src/main/java/com/example/Rental.java
@@ -16,4 +16,27 @@ public class Rental {
     public Movie getMovie() {
 	return _movie;
     }
+
+    // 비디오 종류별 대여료 계산 기능을 빼내어 별도의 함수로 작성
+    public double getCharge() {
+	double result = 0;
+	// 비디오 종류별 대여료 계산
+	switch (getMovie().getPriceCode()) {
+	case Movie.REGULAR:
+	    result += 2;
+	    if (getDaysRented() > 2)
+		result += (getDaysRented() - 2) * 1.5;
+	    break;
+	case Movie.NEW_RELEASE:
+	    result += getDaysRented() * 3;
+	    break;
+	case Movie.CHILDRENS:
+	    result += 1.5;
+	    if (getDaysRented() > 3)
+		result += (getDaysRented() - 3) * 1.5;
+	    break;
+	}
+	return result;
+    }
+
 }
