3cd9328aec6df004693a87080ec46e1834d71954
SHYANG-ENVY\shyang.oblab <shyang.oblab@gmail.com>
2017-06-23 23:05:28 +0900

    테스트 코드 작성
    statement 메소드 리턴값을 출력

--- src/main/java/com/example/RefactoringExampleApplication.java
+++ src/main/java/com/example/RefactoringExampleApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RefactoringExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RefactoringExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(RefactoringExampleApplication.class, args);
+    }
 }
--- src/test/java/com/example/RefactoringExampleApplicationTests.java
+++ src/test/java/com/example/RefactoringExampleApplicationTests.java
@@ -9,8 +9,28 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class RefactoringExampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
+    @Test
+    public void testCustomerStatement() {
+	Movie testMovieRegular = new Movie("test-title-regular", Movie.REGULAR);
+	Movie testMovieNew = new Movie("test-title-new", Movie.NEW_RELEASE);
+	Movie testMovieChildren = new Movie("test-title-children", Movie.CHILDRENS);
+
+	Rental testRental1 = new Rental(testMovieRegular, 1);
+	Rental testRental2 = new Rental(testMovieNew, 2);
+	Rental testRental3 = new Rental(testMovieChildren, 3);
+
+	Customer testCustomer1 = new Customer("test-customer-01");
+	Customer testCustomer2 = new Customer("test-customer-02");
+
+	testCustomer1.addRental(testRental1);
+	System.out.println(testCustomer1.statement());
+	testCustomer1.addRental(testRental2);
+	System.out.println(testCustomer1.statement());
+	testCustomer1.addRental(testRental3);
+	System.out.println(testCustomer1.statement());
+    }
 }
