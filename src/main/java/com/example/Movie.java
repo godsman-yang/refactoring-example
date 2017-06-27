02e0acf34c98726df5406632fb46e5fb16eb1666
SHYANG-ENVY\shyang.oblab <shyang.oblab@gmail.com>
2017-06-27 20:11:04 +0900

    Movie 상속구조 만들기
    switch 문을 재정의
    분류 부호를 상태/전략 패턴으로 전환 - Replace Type Code with State/Strategy
    메서드 이동 - Move Method
    조건문 재정의 - Replace Conditional with Polymorphism

--- src/main/java/com/example/Movie.java
+++ src/main/java/com/example/Movie.java
@@ -5,19 +5,31 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
 	_title = title;
-	_priceCode = priceCode;
+	setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-	return _priceCode;
+	return _price.getPriceCode();
     }
 
     public void setPriceCode(int priceCode) {
-	_priceCode = priceCode;
+	switch (priceCode) {
+	case Movie.REGULAR:
+	    _price = new RegularPrice();
+	    break;
+	case Movie.NEW_RELEASE:
+	    _price = new NewReleasePrice();
+	    break;
+	case Movie.CHILDRENS:
+	    _price = new ChildrensPrice();
+	    break;
+	default:
+	    throw new IllegalArgumentException("가격 코드가 잘못됐습니다.");
+	}
     }
 
     public String getTitle() {
@@ -25,31 +37,10 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-	double result = 0;
-	// 비디오 종류별 대여료 계산
-	switch (getPriceCode()) {
-	case Movie.REGULAR:
-	    result += 2;
-	    if (daysRented > 2)
-		result += (daysRented - 2) * 1.5;
-	    break;
-	case Movie.NEW_RELEASE:
-	    result += daysRented * 3;
-	    break;
-	case Movie.CHILDRENS:
-	    result += 1.5;
-	    if (daysRented > 3)
-		result += (daysRented - 3) * 1.5;
-	    break;
-	}
-	return result;
+	return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-	if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-	    return 2;
-	else
-	    return 1;
+	return _price.getFrequentRenterPoints(daysRented);
     }
-
 }
---
+++ src/main/java/com/example/Price.java
@@ -0,0 +1,57 @@
+package com.example;
+
+public abstract class Price {
+    abstract int getPriceCode();
+
+    abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+	return 1;
+    }
+}
+
+class ChildrensPrice extends Price {
+    int getPriceCode() {
+	return Movie.CHILDRENS;
+    }
+
+    public double getCharge(int daysRented) {
+	double result = 1.5;
+	if (daysRented > 3)
+	    result += (daysRented - 3) * 1.5;
+
+	return result;
+    }
+}
+
+class NewReleasePrice extends Price {
+    int getPriceCode() {
+	return Movie.NEW_RELEASE;
+    }
+
+    public double getCharge(int daysRented) {
+	double result = 3;
+	if (daysRented > 3)
+	    result += (daysRented - 3) * 1.5;
+
+	return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+	return (daysRented > 1) ? 2 : 1;
+    }
+}
+
+class RegularPrice extends Price {
+    int getPriceCode() {
+	return Movie.REGULAR;
+    }
+
+    public double getCharge(int daysRented) {
+	double result = 2;
+	if (daysRented > 2)
+	    result += (daysRented - 2) * 1.5;
+
+	return result;
+    }
+}
